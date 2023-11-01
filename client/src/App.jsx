7134e021a2d8a7d6b2df76d837eef21815a87b0a
Archit Sehgal <architsehgal19@gmail.com>
2023-11-01 06:17:38 +0530

    update added single todo delete btn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 function App() {
@@ -13,21 +15,26 @@ function App() {
       setOutput(JSON.parse(savedData));
     }
   }, []);
-  
+
   const senddata = () => {
     axios.post("http://127.0.0.1:8080/todo", { todo }).then((response) => {
-      console.log("Response Data: ", response.data);
       setOutput(response.data);
       localStorage.setItem("todoData", JSON.stringify(response.data));
     });
   };
   const deletetodos = () => {
     axios.post("http://127.0.0.1:8080/delete").then((response) => {
-      console.log("Response Data: ", response.data);
       setOutput(response.data);
-      localStorage.setItem("todoData", JSON.stringify(response.data))
+      localStorage.setItem("todoData", JSON.stringify(response.data));
     });
   };
+  const deleteSingleTodo = (todoId) => {
+    axios.delete(`http://127.0.0.1:8080/delete/todo/${todoId}`).then((response) => {
+      setOutput(response.data);
+      localStorage.setItem("todoData", JSON.stringify(response.data));
+    });
+  };
+  
 
   return (
     <div className="main flex">
@@ -48,7 +55,23 @@ function App() {
       </Button>
       <div className="output">
         {Array.isArray(output) &&
-          output.map((t, index) => <Alert severity="info" style={{margin:"10px"}} key={index}>{t}</Alert>)}
+          output.map((t, index) => (
+            <div className="mainout flex" key={index}>
+              <Alert
+                className="output"
+                severity="info"
+                style={{ margin: "10px" }}
+              >
+                ID: {t.id}, Task: {t.task}
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => deleteSingleTodo(t.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Alert>
+            </div>
+          ))}
       </div>
     </div>
   );
